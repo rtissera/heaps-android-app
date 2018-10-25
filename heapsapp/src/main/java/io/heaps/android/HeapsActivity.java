8198dc45b2a176441a003183508e1c5193486b07
Romain Tisserand <romain.tisserand@gmail.com>
2018-10-25 11:20:44 +0200

    Initial commit

---
+++ heapsapp/src/main/java/io/heaps/android/HeapsActivity.java
@@ -0,0 +1,22 @@
+package io.heaps.android;
+
+import org.libsdl.app.SDLActivity;
+
+public class HeapsActivity extends SDLActivity {
+
+    // Used to load the native libraries on application startup.
+    static {
+        System.loadLibrary("openal");
+        System.loadLibrary("SDL2");
+        System.loadLibrary("heapsapp");
+    }
+
+    @Override
+    protected String[] getLibraries() {
+        return new String[]{
+                "openal",
+                "SDL2",
+                "heapsapp"
+        };
+    }
+}
